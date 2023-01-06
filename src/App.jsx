d0b65d61dd56f1521044004ed01cad6e9f64cf38
CHRIS BIGLAND <christopherbigland@gmail.com>
2023-01-06 12:21:44 +0000

    added stylings

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,16 @@ const App = () => {
   );
   const [radioValue, setRadioValue] = useState("all");
 
+//   const setPageNum = () => {
+//   const pages = [1, 2, 3, 4, 5]
+
+//   for(let i = 0; i < pages.length; i++) {
+//       console.log(pages[i])
+//       return pages[i]
+//   }
+// }
+// console.log("setPageNum return is ", setPageNum())
+  // "https://api.punkapi.com/v2/beers?page=1&per_page=80"
 
   const updateDisplayedBeers = (searchTerm) => {
     // uses the search text to display relevant beers on the screen
@@ -21,15 +31,19 @@ const App = () => {
     setPageContent(newPageContent);
   };
 
-  const cleanBeers = (beers) => {
-    // adds in a placeholder image to any item with 'null' for an image URL
-    const cleanedBeers = beers.map((beer) => {
-      if (beer.props.beer.image_url === null) {
-        beer.props.beer.image_url = './images/brewdog.png'
-      }
-    })
-    return cleanedBeers
-  }
+  // const cleanBeers = (beers) => {
+  //   // adds in a placeholder image to any item with 'null' for an image URL
+  //   const cleanedBeers = beers.map((beer) => {
+  //     if (beer.props.beer.image_url === null) {
+  //       beer.props.beer.image_url = './images/brewdog.png'
+  //       return beer // ??
+  //     }
+  //     else {
+  //       return beer
+  //     }
+  //   })
+  //   return cleanedBeers
+  // }
 
   const getBeers = () => {
     // fetches beers from API and sets them to state 'beers'
@@ -72,9 +86,8 @@ const App = () => {
 export default App;
 
 // TASK LIST
-// if no image - put in a placeholder image.
-// get multiple (more than 80) showing on page.
+// if no image - put in a placeholder image. Do this after I've worked out how to display all beers on page (as first 80 all have images)
+// get multiple (more than 80) showing on page. For loop for this - fetch data from page one then two then three
 // write tests
-// sort out feedback panel
 // write readme
-// make feedback panel reusable (pass content down as props)
+// styling
