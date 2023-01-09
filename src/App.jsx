440c845853cbea83b50b3023e90180322de5c5a4
CHRIS BIGLAND <christopherbigland@gmail.com>
2023-01-09 11:43:11 +0000

    working on fetching api data multiple times. Issue with empty array showing in console but can click and get data

--- src/App.jsx
+++ src/App.jsx
@@ -35,50 +35,55 @@ const App = () => {
     return cleanedBeers;
   };
 
-  const getBeers = () => {
-    // fetches beers from API and sets them to state 'beers'
-    fetch(pageContent)
-      .then((response) => response.json())
-      .then((jsonResponse) => {
-        setBeers(cleanBeers(jsonResponse));
-      });
-  };
-
   // const getBeers = () => {
-  //   for (let index = 1; index < 6; index++) {
-  //     // fetches beers from API and sets them to state 'beers'
-  //     fetch(`https://api.punkapi.com/v2/beers?page=${index}&per_page=80`)
-  //       .then((response) => response.json())
-  //       .then((jsonResponse) => {
-  //         setApiBeersArr(
-  //           apiBeersArr.length < 5
-  //             ? apiBeersArr.push(jsonResponse)
-  //             : apiBeersArr
-  //         );
-  //       });
-  //   }
+  //   // fetches beers from API and sets them to state 'beers'
+  //   fetch(pageContent)
+  //     .then((response) => response.json())
+  //     .then((jsonResponse) => {
+  //       setBeers(cleanBeers(jsonResponse));
+  //     });
   // };
 
-  // //a function to combine an array of 5 arrays. 
-  // const combineData = (data) => {
-  //   console.log("apiBeersArr is now called 'data', is in combineData function and the value is ", data);
-  //   console.log(data[0].concat(data[1], data[2], data[3], data[4]))
-  // }
+  const getBeers = () => {
+    let beerArr = [];
+    for (let index = 1; index < 6; index++) {
+      // fetches beers from API and sets them to state 'beers'
+      fetch(`https://api.punkapi.com/v2/beers?page=${index}&per_page=80`)
+        .then((response) => response.json())
+        .then((jsonResponse) => {
+          beerArr.push(jsonResponse)
+          // console.log(jsonResponse)
+          // setApiBeersArr(
+          //   apiBeersArr.length < 5
+          //     ? apiBeersArr.push(jsonResponse)
+          //     : apiBeersArr
+          // );
+        });
+    }
+
+    // try setting one to state. Issue here appears to be that array is showing as empty in console however when we click into it, it has 5 items. 
+    console.log(beerArr.length)
+    const newBeerArr = beerArr
+    console.log(newBeerArr)
+    // setApiBeersArr(newBeerArr)
+    // const joinedBeerArr = [...beerArr[0], ...beerArr[1], ...beerArr[2], ...beerArr[3], ...beerArr[4]]
+    // setApiBeersArr(joinedBeerArr)
+  };
+
+  //a function to combine an array of 5 arrays. 
+  const combineData = (data) => {
+    console.log("apiBeersArr is now called 'data', is in combineData function and the value is ", data);
+    console.log(data[0].concat(data[1], data[2], data[3], data[4]))
+  }
 
 
 
   useEffect(() => {
     // calls getBeers() only once the pageContent (state containing relevant fetch URL) has been set.
     getBeers();
-    console.log(beers);
+    // console.log(beers);
+    // combineData(apiBeersArr)
   }, [pageContent]);
-
-
-  // useEffect(() => {
-  //   // calls getBeers() only once the pageContent (state containing relevant fetch URL) has been set.
-  //   console.log("apiBeersArr is ", apiBeersArr)
-  //   combineData(apiBeersArr)
-  // }, [apiBeersArr]);
 
   return (
     <>
--- test.js
+++ test.js
@@ -1,26 +1,27 @@
-let arrays = [
-  [
-    { name: "john", age: 22 },
-    { name: "sally", age: 32 },
-    { name: "jeff", age: 56 },
-  ],
-  [
-    { name: "mary", age: 53 },
-    { name: "jemima", age: 52 },
-    { name: "ray", age: 66 },
-  ],
-  [
-    { name: "ron", age: 12 },
-    { name: "helen", age: 92 },
-    { name: "simon", age: 51 },
-  ],
-];
+// let arrays = [
+//   [
+//     { name: "john", age: 22 },
+//     { name: "sally", age: 32 },
+//     { name: "jeff", age: 56 },
+//   ],
+//   [
+//     { name: "mary", age: 53 },
+//     { name: "jemima", age: 52 },
+//     { name: "ray", age: 66 },
+//   ],
+//   [
+//     { name: "ron", age: 12 },
+//     { name: "helen", age: 92 },
+//     { name: "simon", age: 51 },
+//   ],
+// ];
 
 
-arrays = arrays.reduce((a, b) => a.concat(b), []);
-console.log(arrays)
+// arrays = arrays.reduce((a, b) => a.concat(b), []);
+// console.log(arrays)
 
 
 // console.log(arrays[0].concat(arrays[1], arrays[2]));
 
 // console.log(arrays.flat())
+
